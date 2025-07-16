c78498357673be51af39e4a4c380932ef09d7549
Charley <charley.geoffroy@protonmail.com>
2025-07-16 08:48:20 +0200

    feat:implement structured event system with comprehensive network and app event handling

--- src/event.rs
+++ src/event.rs
@@ -1,22 +1,68 @@
 use std::sync::{Arc, Mutex};
 
+use crate::endpoint::Endpoint;
+
 #[derive(Clone)]
 pub enum SocketEngineEvent {
-    Reception(Vec<u8>),
-    Sent(String),
-    SentError((String, String)), // err msg, uuid
-                                 //TODO: Info / Error
+    Info(EventSocket),
+    Error(ErrorEventSocket),
+}
+
+#[derive(Clone, Debug)]
+pub enum EventSocket {
+    Tcp(TcpEvent),
+    Udp(UdpEvent),
+    General(GeneralSocketEvent),
+}
+
+#[derive(Clone, Debug)]
+pub enum GeneralSocketEvent {
+    DataReceived(Vec<u8>, Endpoint),
+    DataSent(String, Endpoint),
+    
+}
+
+#[derive(Clone, Debug)]
+pub enum ErrorEventSocket {
+    Tcp(TcpErrorEvent),
+    General(GeneralSocketErrorEvent),
+}
+#[derive(Clone, Debug)]
+pub enum GeneralSocketErrorEvent {
+    ConnectionError(String, Endpoint),
+    SendFailed(String, String, Endpoint),
+    ReceiveFailed(String, Endpoint),
+}
+
+#[derive(Clone, Debug)]
+pub enum TcpErrorEvent {
+    ConnectionRefused(String, Endpoint),
+    ConnectionTimeout(String, Endpoint),
+}
+
+#[derive(Clone, Debug)]
+pub enum UdpEvent {
+    PacketSizeSent(u64),
+    PacketSizeReceived(u64),
+}
+
+#[derive(Clone, Debug)]
+pub enum TcpEvent {
+    ConnectionEstablished(String),
+    
+    ListenerStarted(String),
+    ConnectionClosed(String),
 }
 
 pub trait EngineObserver: Send + Sync {
-    fn notify(&mut self, event: SocketEngineEvent);
+    fn on_engine_event(&mut self, event: SocketEngineEvent);
 }
 
-pub fn notify_all(
+pub fn notify_all_observers(
     observers: &Vec<Arc<Mutex<dyn EngineObserver + Send + Sync>>>,
     event: &SocketEngineEvent,
 ) {
     for obs in observers {
-        obs.lock().unwrap().notify(event.clone());
+        obs.lock().unwrap().on_engine_event(event.clone());
     }
 }
